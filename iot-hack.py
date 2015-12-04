0cd3a8388e5e7320a8b3ee15a352be19b85527ed
Daniel Wärnå <daniel@danielwarna.com>
2015-12-04 23:41:38 +0200

    Printing data

--- iot-hack.py
+++ iot-hack.py
@@ -7,34 +7,31 @@ app = Flask(__name__)
 
 app.config.from_object(config.DevelopmentConfig)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def hello_world():
     return 'Hello World!'
 
 
-@app.route('/api/', methods=['POST', 'GET'])
+#Hit kommer on event requestar
+@app.route('/api', methods=['POST', 'GET'])
 def api():
-    print "API request" 
+    print "API request"
     print request.get_json()
     return "test"
 
-@app.route('/v2/events/', methods=['POST', 'GET'])
+#Hit kommer generella cloud requests
+@app.route('/v2/events', methods=['POST', 'GET'])
 def thingsee_in():
     print "Thingsee endpoint"
     print request.get_json()
     return "test"
-   
+
 
 @app.route('/debug/')
 def debug_mode():
     assert current_app.debug == False, "Don't panic! You're here by request of debug()"
     return "aa"
 
-@app.route('/<path:path>/', methods=['POST', 'GET'])
-def matchall(path):
-    print "catch all the things"
-    print request.get_json()
-    return "test"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
