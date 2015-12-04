ef99e31e96424eed770de47a1ddd01308bca5fa3
Daniel Wärnå <daniel@danielwarna.com>
2015-12-04 21:36:38 +0200

    Update gitignore and requirements Add config file

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+# Get application base dir.
+_basedir = os.path.abspath(os.path.dirname(__file__))
+print _basedir
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'mysecretkeyvalue'
+    TEMPLATE_FOLDER = "templates"
+    
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    RELOAD = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'dev.db')
+    #DATABASE = '/tmp/flaskr.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
