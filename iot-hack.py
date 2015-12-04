9981d5c26ecbb7929f16098796ce1e19c3015def
Magnus <gulin.magnus@gmail.com>
2015-12-04 23:41:12 +0200

    we can haz on-the-fly-generated graphs from flask

--- iot-hack.py
+++ iot-hack.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import current_app, request
+from flask import make_response, send_file
+import cStringIO
 
 import config
 
@@ -22,11 +24,35 @@ def thingsee_in():
     print "Thingsee endpoint"
     return "test"
    
-
+"""
 @app.route('/debug/')
 def debug_mode():
     assert current_app.debug == False, "Don't panic! You're here by request of debug()"
     return "aa"
+"""
+
+
+@app.route('/graph/')
+def debug_mode():
+    import matplotlib.pyplot as plt
+    #plt.plot([1, 2, 3, 4])
+    #plt.ylabel('some numbers')
+    # plt.show()
+    # return "aa"
+    #return plt.show()
+    #response = make_response(plt.show())
+    RAM = cStringIO.StringIO()
+    CHART = plt.figure()
+
+
+    CHART.savefig(RAM, format='png')
+    RAM.seek(0)
+    return send_file(RAM, mimetype='image/png')
+
+    # response.headers['Content-Type'] = 'image/jpeg'
+    # response.headers['Content-Disposition'] = 'attachment; filename=img.jpg'
+    # return response
+
 
 @app.route('/<path:path>', methods=['POST', 'GET'])
 def matchall(path):
@@ -34,4 +60,4 @@ def matchall(path):
     return "test"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
