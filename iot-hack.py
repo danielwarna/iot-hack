e80a9e0c4efc6789f833fad006c7ee5d67e6c01a
Daniel Wärnå <daniel@danielwarna.com>
2015-12-04 22:18:58 +0200

    Testing api endpoints

--- iot-hack.py
+++ iot-hack.py
@@ -17,10 +17,21 @@ def thingsee_in():
     print "API request"
     return "test"
 
+@app.route('/v2/events/', methods=['POST', 'GET'])
+def thingsee_in():
+    print "Thingsee endpoint"
+    return "test"
+   
+
 @app.route('/debug/')
 def debug_mode():
     assert current_app.debug == False, "Don't panic! You're here by request of debug()"
     return "aa"
+
+@app.route('/<path:path>', methods=['POST', 'GET'])
+def matchall(path):
+    print "catch all the things"
+    return "test"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
