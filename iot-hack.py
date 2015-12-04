40e9938411ef6b01f59d9d2c438a1c8c63b1ffa3
Daniel Wärnå <daniel@danielwarna.com>
2015-12-04 21:57:46 +0200

    Add debug view

--- iot-hack.py
+++ iot-hack.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import current_app
 
 import config
 
@@ -14,6 +15,11 @@ def hello_world():
 @app.route('/api/')
 def thingsee_in():
     return "APIII"
+
+@app.route('/debug/')
+def debug_mode():
+    assert current_app.debug == False, "Don't panic! You're here by request of debug()"
+    return "aa"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
