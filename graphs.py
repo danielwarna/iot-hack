2c894df24a850ded91d312c21b2731c2629a833d
Magnus <gulin.magnus@gmail.com>
2015-12-05 00:06:19 +0200

    put graph logic in graph.py

---
+++ graphs.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+from flask import send_file
+import cStringIO
+
+
+def serve_graph(sensor1=None, sensor2=None, sensor3=None):
+    ram_file = cStringIO.StringIO()
+    # plt.plot([1, 2, 3, 4])
+    plt.ylabel('Jytky/s')
+    plt.xlabel('Herra Soini')
+    if sensor1:
+        plt.plot(sensor1)
+    if sensor2:
+        plt.plot(sensor2)
+    if sensor3:
+        plt.plot(sensor3)
+
+    plt.savefig(ram_file, format='png')
+    ram_file.seek(0)
+    return send_file(ram_file, mimetype='image/png')
